5aad8aa6f17a3d2809f382ff94bb18cfc1127bbf
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-07-30 14:52:26 +0800

    fix: types

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,39 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import { builtinModules } from "module";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default {
-	input: 'src/index.ts',
-	output: [{
-		format: 'esm',
-    exports: 'named',
-		file: pkg.module,
-		sourcemap: false,
-	}, {
-		format: 'cjs',
-    exports: 'named',
-		file: pkg.main,
-		sourcemap: false,
-		esModule: false,
-	}, {
-		name: pkg['umd:name'] || pkg.name,
-		format: 'umd',
-    exports: 'named',
-		file: pkg.unpkg,
-		sourcemap: false,
-		esModule: false,
-		plugins: [
-			terser()
-		]
-	}],
+	input: "src/index.ts",
+	output: [
+		{
+			format: "esm",
+			exports: "named",
+			file: pkg.module,
+			sourcemap: false,
+		},
+		{
+			format: "cjs",
+			exports: "named",
+			file: pkg.main,
+			sourcemap: false,
+			esModule: false,
+		},
+		{
+			name: pkg["umd:name"] || pkg.name,
+			format: "umd",
+			exports: "named",
+			file: pkg.unpkg,
+			sourcemap: false,
+			esModule: false,
+			plugins: [terser()],
+		},
+	],
 	external: [
-		...require('module').builtinModules,
+		...builtinModules,
 		...Object.keys(pkg.dependencies || {}),
 		...Object.keys(pkg.peerDependencies || {}),
 	],
-	plugins: [
-		resolve(),
-		typescript({
-			useTsconfigDeclarationDir: true
-		})
-	]
-}
+	plugins: [resolve(), typescript()],
+};
